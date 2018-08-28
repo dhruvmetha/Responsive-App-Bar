fa0ab58009c66f802642801d90ab5df393f13352
dhruvmetha <dhruv.metha@gmail.com>
2018-08-28 15:07:56 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ResAppBar from './resappbar/ResAppBar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <ResAppBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/resappbar/ResAppBar.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  AppBar, Toolbar, Typography, List, ListItem,
+  withStyles, Grid, SwipeableDrawer
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styleSheet = {
+  list : {
+    width : 200,
+  },
+  padding : {
+    paddingRight : 30,
+    cursor : "pointer",
+  },
+
+  sideBarIcon : {
+    padding : 0,
+    color : "white",
+    cursor : "pointer",
+  }
+}
+
+class ResAppBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {drawerActivate:false, drawer:false};
+    this.createDrawer = this.createDrawer.bind(this);
+    this.destroyDrawer = this.destroyDrawer.bind(this);
+  }
+
+  componentWillMount(){
+    if(window.innerWidth <= 600){
+      this.setState({drawerActivate:true});
+    }
+
+    window.addEventListener('resize',()=>{
+      if(window.innerWidth <= 600){
+        this.setState({drawerActivate:true});
+      }
+      else{
+        this.setState({drawerActivate:false})
+      }
+    });
+  }
+
+  //Small Screens
+  createDrawer(){
+    return (
+      <div>
+        <AppBar >
+          <Toolbar>
+            <Grid container direction = "row" justify = "space-between" alignItems="center">
+              <MenuIcon
+                className = {this.props.classes.sideBarIcon}
+                onClick={()=>{this.setState({drawer:true})}} />
+
+              <Typography color="inherit" variant = "headline">Title</Typography>
+              <Typography color="inherit" variant = "headline"></Typography>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+
+        <SwipeableDrawer
+         open={this.state.drawer}
+         onClose={()=>{this.setState({drawer:false})}}
+         onOpen={()=>{this.setState({drawer:true})}}>
+
+           <div
+             tabIndex={0}
+             role="button"
+             onClick={()=>{this.setState({drawer:false})}}
+             onKeyDown={()=>{this.setState({drawer:false})}}>
+
+            <List className = {this.props.classes.list}>
+               <ListItem key = {1} button divider> Option 1 </ListItem>
+               <ListItem key = {2} button divider> Option 2 </ListItem>
+               <ListItem key = {3} button divider> Option 3 </ListItem>
+             </List>
+
+         </div>
+       </SwipeableDrawer>
+
+      </div>
+    );
+  }
+
+  //Larger Screens
+  destroyDrawer(){
+    const {classes} = this.props
+    return (
+      <AppBar>
+        <Toolbar>
+          <Typography variant = "headline" style={{flexGrow:1}} color="inherit" >Title</Typography>
+          <Typography variant = "subheading" className = {classes.padding} color="inherit" >OPTION 1</Typography>
+          <Typography variant = "subheading" className = {classes.padding} color="inherit" >OPTION 2</Typography>
+          <Typography variant = "subheading" className = {classes.padding} color="inherit" >OPTION 3</Typography>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.drawerActivate ? this.createDrawer() : this.destroyDrawer()}
+      </div>
+    );
+  }
+}
+
+ResAppBar.propTypes = {
+  classes : PropTypes.object.isRequired
+};
+
+
+
+export default withStyles(styleSheet)(ResAppBar);
